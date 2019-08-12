b3158a80349c450871ff6188dd892532570a2634
Adan Rosales <adan.rosales_c@janrain.com>
2019-08-12 13:14:45 -0500

    "first commit"

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDisplay from './SeasonDisplay'
+import Spinner from './Spinner'
+
+class App extends React.Component {
+    state = { 
+        lat: null, 
+        long: null,
+         errorMessage:null 
+    };
+
+    componentDidMount(){
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat:position.coords.latitude, long:position.coords.longitude }),
+            err => this.setState({ errorMessage: err.message})
+        );
+
+    }
+
+    componentDidUpdate(){
+        console.log('My component aws just updated.');
+    }
+    renderBody()
+    {
+        if (this.state.errorMessage){
+            return <div> Error:<br/>{this.state.errorMessage} </div>
+        }
+        if (!this.state.errorMessage && this.state.lat){
+           return <SeasonDisplay lat={this.state.lat} long={this.state.long}/>;
+        }
+        return <Spinner />
+    }
+     render(){
+        return( <div className="ui box">
+        { this.renderBody() }
+            </div>);
+}
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
